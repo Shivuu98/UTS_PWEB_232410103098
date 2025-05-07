2e3df464164b0393a1b1255081cda600878a783d
Muhammad Rafi Kurniawa <muhammadrafi9b24@gmail.com>
2025-05-07 18:06:24 +0700

    update page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -29,7 +29,6 @@
                 </svg>
             </button>
 
-            <!-- Profile dropdown -->
             <div class="relative ml-3">
                 <div>
                 <button type="button"  @click="isOpen = !isOpen" class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
@@ -51,15 +50,12 @@
             </div>
         </div>
         <div class="-mr-2 flex md:hidden">
-            <!-- Mobile menu button -->
             <button type="button" @click="isOpen = !isOpen" class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden" aria-controls="mobile-menu" aria-expanded="false">
             <span class="absolute -inset-0.5"></span>
             <span class="sr-only">Open main menu</span>
-            <!-- Menu open: "hidden", Menu closed: "block" -->
             <svg :class="{ 'hidden': isOpen, 'block': !isOpen }" class="block size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
-            <!-- Menu open: "block", Menu closed: "hidden" -->
             <svg :class="{ 'block': isOpen, 'hidden': !isOpen }" class="hidden size-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
             </svg>
@@ -68,13 +64,14 @@
         </div>
     </div>
 
-    <!-- Mobile menu, show/hide based on menu state. -->
     <div x-show = "isOpen" class="md:hidden" id="mobile-menu">
         <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
-        <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-        <x-nav-link href='/dashboard' :active="request()->is('dashboard')">Dashboard</x-nav-link>
-        <x-nav-link href='/pengelolaan' :active="request()->is('pengelolaan')">Pengelolaan</x-nav-link>
-        <x-nav-link href='/profile' :active="request()->is('profile')">Profile</x-nav-link>
+            <x-nav-link href="{{ route('dashboard', ['username' => request()->query('username')]) }}" 
+                :active="request()->is('dashboard')">Dashboard</x-nav-link>
+            <x-nav-link href="{{ route('pengelolaan', ['username' => request()->query('username')]) }}" 
+                :active="request()->is('pengelolaan')">Pengelolaan</x-nav-link>
+            <x-nav-link href="{{ route('profile', ['username' => request()->query('username')]) }}" 
+                :active="request()->is('profile')">Profile</x-nav-link>
         </div>
     </div>
 </nav>
